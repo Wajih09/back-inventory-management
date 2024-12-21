f3493d140579af6fd34cd43b16ded7a173a1ae2b
Wajih09 <112098136+Wajih09@users.noreply.github.com>
2024-12-21 22:48:34 +0100

    Update Adresse.java

--- src/main/java/com/tech/gestiondestock/models/Adresse.java
+++ src/main/java/com/tech/gestiondestock/models/Adresse.java
@@ -9,10 +9,9 @@ import javax.persistence.Embeddable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode
-@Embeddable //v5 ca sera un champ composé ultérieurement mais c pas pour tous les classes comme id et dateCreation dans AbstractEntity
-@Builder //v6 c une classe qui permet de construire un objet en exposant des methodes de meme nom des attributs et renvoyant les memes methodes et build() qui prend les attributs qui sont passés en parametres pour creer un objet
-//v8 normalment on va enlever le builder dans les entities et on laisse seulement dans les dto, car on a pas accès au champ id hérité de AbstractEntity avec la builder().
-public class Adresse { //v5
+@Embeddable
+@Builder
+public class Adresse {
 
     @Column(name = "adresse1")
     private String adresse1;
