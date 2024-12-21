82e0fd1308f46091ab9db1f262b46c8a7cf988e6
Wajih09 <112098136+Wajih09@users.noreply.github.com>
2024-12-21 23:02:02 +0100

    Update ArticleDao.java

--- src/main/java/com/tech/gestiondestock/repository/ArticleDao.java
+++ src/main/java/com/tech/gestiondestock/repository/ArticleDao.java
@@ -11,13 +11,13 @@ import java.util.Optional;
 public interface ArticleDao extends JpaRepository<Article, Integer> {
 
     @Query("select a from Article a where a.codeArticle = :code and a.designation = :designation")
-    List<Article> findByCustomQuery(@Param("code") String c, @Param("designation") String d);//v11 min28
+    List<Article> findByCustomQuery(@Param("code") String c, @Param("designation") String d);
 
-    @Query(value = "select * from article where code = :code", nativeQuery = true) // pas besoin de convertir la requete, elle est deja en native sql par expl
-    List<Article> findByCustomNativeQuery(String code); //v11 no need for @Param("code") if you put the same name
+    @Query(value = "select * from article where code = :code", nativeQuery = true)
+    List<Article> findByCustomNativeQuery(String code);
 
 
-    List<Article> findByCodeArticleIgnoreCaseAndDesignationIgnoreCase(String codeArticle, String designation); //v11 IgnoreCase means we cans search using low case or upper case
+    List<Article> findByCodeArticleIgnoreCaseAndDesignationIgnoreCase(String codeArticle, String designation);
 
     Optional<Article> findByCodeArticle(String codeArticle);
 }
