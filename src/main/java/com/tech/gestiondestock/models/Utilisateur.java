f527441d81dfface3e28302e446dee61c93e266d
Wajih09 <112098136+Wajih09@users.noreply.github.com>
2024-12-21 23:00:06 +0100

    Update Utilisateur.java

--- src/main/java/com/tech/gestiondestock/models/Utilisateur.java
+++ src/main/java/com/tech/gestiondestock/models/Utilisateur.java
@@ -13,7 +13,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Builder //v6 pour faire une copy (dto) contient seulement des champs spécéfiques qu'on veut l'exposer à une APi externe (angular ou autres)
+@Builder
 @EqualsAndHashCode(callSuper = true)
 @Table(name = "utilisateur")
 public class Utilisateur extends AbstractEntity {
@@ -30,25 +30,17 @@ public class Utilisateur extends AbstractEntity {
     @Column(name = "motdepasse")
     private String motDePasse;
 
-    @Embedded // v5 cad champ composé
-    private Adresse adresse; //v5 puisque c composé de 3 sous-adresse et va se répété dans plusieurs entitées donc on va créer une classe adresse
+    @Embedded 
+    private Adresse adresse; 
 
-    @Column(name = "email", unique = true) //min44 https://www.youtube.com/watch?v=xqhdRrFzLFY&list=PL41m5U3u3wwl5FoM2Y5gIu1Q-Wr5ascD_&index=1
+    @Column(name = "email", unique = true)
     private String email;
 
     @Column(name = "photo")
     private String photo;
 
-//    @Column(name = "identreprise")
-//    private Integer idEntreprise;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "entreprise")
-//    private Entreprise entreprise;
-    
-//
-//    @OneToMany(mappedBy = "utilisateur")
-//    private List<Roles> roles;
+    @OneToMany(mappedBy = "utilisateur")
+    private List<Roles> roles;
     
     @ManyToOne (cascade = CascadeType.PERSIST)
     @JoinColumn(name = "identreprise")
