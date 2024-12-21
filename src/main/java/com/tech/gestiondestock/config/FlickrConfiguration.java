bee1cce4d38d9777f69df400130603701c533dd0
Wajih09 <112098136+Wajih09@users.noreply.github.com>
2024-12-21 21:08:06 +0100

    Update FlickrConfiguration.java

--- src/main/java/com/tech/gestiondestock/config/FlickrConfiguration.java
+++ src/main/java/com/tech/gestiondestock/config/FlickrConfiguration.java
@@ -19,9 +19,9 @@ import java.io.IOException;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
 
-@Configuration //v15 min25 dire a srping que c une classe de configuration donc il faut executer tous les beans à l'interieur
-public class FlickrConfiguration {// v15 on peut supprimer cette classe apres une premiere excecution
-    @Value("${flickr.apiKey}")//v15 min 16 spring va chercher cette valeur dans application.yml
+@Configuration
+public class FlickrConfiguration {
+    @Value("${flickr.apiKey}")
     private String apiKey;
 
     @Value("${flickr.apiSecret}")
@@ -34,38 +34,7 @@ public class FlickrConfiguration {// v15 on peut supprimer cette classe apres un
     @Value("${flickr.appSecret}")
     private String appSecret;
 
-    //v15 min26
-    /*@Bean //1ere methode
-    public Flickr getFlickr() throws IOException, ExecutionException, InterruptedException, FlickrException {
-        Flickr flickr = new Flickr(apiKey, apiSecret, new REST());
-        OAuth10aService service = new ServiceBuilder(apiKey)
-                .apiSecret(apiSecret)
-                .build(FlickrApi.instance(FlickrApi.FlickrPerm.DELETE)); //delele veut dire accorder tous les droits à mon application
-
-        final Scanner scanner = new Scanner(System.in);
-        final OAuth1RequestToken requestToken = service.getRequestToken();
-        final String authUrl = service.getAuthorizationUrl(requestToken);// url qui va nous permettre d'autoriser notre application d'utiliser l'api flickr
-
-        System.out.println(authUrl);
-        System.out.println("Paste it here >> ");
-
-        final String authVerifier = scanner.nextLine();
-
-        OAuth1AccessToken accessToken = service.getAccessToken(requestToken, authVerifier);
-
-        System.out.println(accessToken.getToken());
-        System.out.println(accessToken.getTokenSecret());
-
-        Auth auth = flickr.getAuthInterface().checkToken(accessToken);
-
-        System.out.println("---------------------------------------");
-        System.out.println(auth.getToken());
-        System.out.println(auth.getTokenSecret());
-
-        return flickr;
-    }*/
-
-    @Bean //v15 min39 2eme methode
+    @Bean
     public Flickr getFlickrAutowired(){
         Flickr flickr = new Flickr(apiKey, apiSecret, new REST());
         Auth auth = new Auth();
