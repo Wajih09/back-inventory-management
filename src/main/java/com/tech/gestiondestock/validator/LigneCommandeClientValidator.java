d2b2545abc9592b3a55b0c4fc1832665f863cff4
Wajih09 <112098136+Wajih09@users.noreply.github.com>
2024-12-21 23:17:48 +0100

    Update LigneCommandeClientValidator.java

--- src/main/java/com/tech/gestiondestock/validator/LigneCommandeClientValidator.java
+++ src/main/java/com/tech/gestiondestock/validator/LigneCommandeClientValidator.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 public class LigneCommandeClientValidator {
 
-  // TODO to be implemented
   public static List<String> validate(LigneCommandeClientDto dto) {
     List<String> errors = new ArrayList<>();
     return errors;
